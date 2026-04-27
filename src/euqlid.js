b998073ad3d7740f3ab8da2c7f9972366aedb8e0
AndriyP0 <andreopopovich@gmail.com>
2026-04-28 00:25:25 +0300

    euqlid algorithm with try-catch

--- src/euqlid.js
+++ src/euqlid.js
@@ -1,22 +1,31 @@
-'use strict';
+"use strict";
 
-const euqlid = (val1,val2) =>{
-    if(val1 !== 0 && val2 !== 0){
-        val1 = Math.round(val1);
-        val2 = Math.round(val2);
-       
-    if((val1 % val2) === 0){
-            return val2
+const euqlid = (val1, val2) => {
+  try {
+    if (typeof val1 !== "number" || typeof val2 !== "number") {
+      throw new Error("Аргументами повинні бути числами");
     }
-    if((val1 % val2) > 0){
-       
-       return euqlid(val2 ,val1 % val2)
+    if (val1 <= 0 || val2 <= 0) {
+      throw new Error("Аргументами повинні бути додатними числами");
     }
-    else re
-}}
+
+    val1 = Math.round(val1);
+    val2 = Math.round(val2);
+
+    if (val1 % val2 === 0) {
+      return val2;
+    }
+    return euqlid(val2, val1 % val2);
+  } catch (error) {
+    console.error("Помилка:", error.message);
+    return null;
+  }
+};
+
 // =============TEST=============
-console.log(euqlid(20, 6))
+console.log(euqlid(20, 6));
 console.log(euqlid(111, 17));
 console.log(euqlid(2044, 60));
 console.log(euqlid(10003, 99));
 console.log(euqlid(20, 4));
+console.log(euqlid(-3, 11));
